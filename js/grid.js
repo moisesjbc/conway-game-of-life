6784ef81f5c9dd37a69a8bc7d65b0874e5a8ac2f
Moisés J. Bonilla Caraballo <moisesjbc@gmail.com>
2016-04-14 12:18:51 +0100

    Added method Grid.setCellAlive().

--- js/grid.js
+++ js/grid.js
@@ -7,7 +7,10 @@ function Grid(game_width, game_height, size)
     this.cells = [];
     for(var i=0; i<this.size; i++) {
         this.cells[i] = new Array(this.size);
-    }   
+    }
+    this.setCellAlive(5, 10, true);
+    this.setCellAlive(5, 11, true);
+    this.setCellAlive(5, 9, true);
 }
 
 Grid.prototype.render = function(graphics)
@@ -17,7 +20,20 @@ Grid.prototype.render = function(graphics)
         var y = row * this.cell_height;
         for(var column=0; column<this.size; column++){
             var x = column * this.cell_width;
+            if(this.cells[row][column] == true){
+                graphics.beginFill(0x000000);
+            }else{
+                graphics.beginFill(0xFFFFFF);
+            }
             graphics.drawRect(x, y, this.cell_width, this.cell_height);
+            graphics.endFill();
         }
     }
 }
+
+
+Grid.prototype.setCellAlive = function(row, column, alive)
+{
+    this.cells[row][column] = alive;
+}
+
